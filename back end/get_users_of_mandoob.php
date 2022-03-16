2ce168470e6a87c957d56c4078b986f7905aea17
Hussein-Adel <ha6105014@gmail.com>
2022-03-16 16:31:59 +0200

    change password

--- back end/get_users_of_mandoob.php
+++ back end/get_users_of_mandoob.php
@@ -7,7 +7,7 @@ $host = "SG2NWPLS14SQL-v09.shr.prod.sin2.secureserver.net";
 $username="lifenew";
 $password="Mido@445544";
 $database="newlife2020";
-$connectionInfo = array( "Database"=>"newlife2020", "UID"=>"lifenew", "PWD"=>"dd5*uT02","CharacterSet" => "UTF-8");
+$connectionInfo = array( "Database"=>"newlife2020", "UID"=>"lifenew", "PWD"=>"Mido@445544","CharacterSet" => "UTF-8");
 $conn = sqlsrv_connect($host,$connectionInfo);
 		 $sql;
 		$MandoobID = $_GET['MandoobID'];
